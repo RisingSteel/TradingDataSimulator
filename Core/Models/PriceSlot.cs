4683c1319e9cd47cfab564e2d0a18e9505d1ba2a
MihhaiL Kuznetsov <mihhhailkuz@gmail.com>
2025-08-12 20:14:41 +0300

    Added main host and core logic for generating and getting prices

---
+++ Core/Models/PriceSlot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public sealed class PriceSlot(int capacity)
+    {
+        private readonly object _lock = new();
+        public readonly Queue<PriceTick> _slotBuffer = new Queue<PriceTick>(capacity);
+        public PriceTick _lastTick { get; set; }
+
+        public object Lock()
+        {
+            return _lock;
+        }
+    }
+}
