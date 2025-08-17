b6423f53f501923f9c092d1798bfc6829dca6a9c
MihhaiL Kuznetsov <mihhhailkuz@gmail.com>
2025-08-17 12:26:51 +0300

    moved initial parameters to appsettings.json

---
+++ MainHost/PriceGeneratorOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace MainHost
+{
+    public sealed class PriceGeneratorOptions
+    {
+        public Dictionary<string, decimal> InitialPrices { get; init; } = new();
+        public int UpdateTime { get; init; } = 5000;
+        public int PriceBufferCapacity { get; init; } = 10;
+
+    }
+}
